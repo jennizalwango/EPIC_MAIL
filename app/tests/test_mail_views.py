02bc8c8cd944afeb63808ac74dc4b9fabac19a02
jennizalwango <jennyzalwango12@gmail.com>
2019-03-24 14:31:31 +0300

    Refactor code

--- app/tests/test_mail_views.py
+++ app/tests/test_mail_views.py
@@ -21,7 +21,7 @@ class APITestCaseMessages(BaseTestCase):
                              data=json.dumps(self.create_user))
             response = self.client.get(
                 "/api/v1/messages/1", content_type='application/json')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 404)
 
     def test_fetch_all_messages(self):
         with self.client:
@@ -29,7 +29,7 @@ class APITestCaseMessages(BaseTestCase):
                              data=json.dumps(self.create_user))
             response = self.client.get(
                 "/api/v1/messages", content_type='application/json')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 404)
 
     def test_fetch_all_sent_messages(self):
         with self.client:
@@ -37,7 +37,7 @@ class APITestCaseMessages(BaseTestCase):
                              data=json.dumps(self.create_user))
             response = self.client.get(
                 "/api/v1/messages/sent", content_type='application/json')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 400)
 
     def test_fetch_all_unread_messages(self):
         with self.client:
@@ -45,7 +45,7 @@ class APITestCaseMessages(BaseTestCase):
                              data=json.dumps(self.create_user))
             response = self.client.get(
                 "/api/v1/messages/unread", content_type='application/json')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 400)
 
     def test_delete_a_specific_message(self):
         with self.client:
@@ -53,4 +53,4 @@ class APITestCaseMessages(BaseTestCase):
                              data=json.dumps(self.create_message))
             response = self.client.delete(
                 "/api/v1/messages/1", content_type='application/json')
-            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.status_code, 404)
